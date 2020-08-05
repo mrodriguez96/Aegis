f4410ab070230b4abb0a17e95346099fb9e508ce
Marcos Rodriguez <mrodriguezx777@gmail.com>
2020-08-05 17:20:30 -0400

    Create index.php

---
+++ Code/Aegis/index.php
@@ -0,0 +1,27 @@
+<!--
+Webpage Name:index.php
+Main Developer: CJ Conti
+Primary Function:The home page for users that describes what the website does.
+-->
+<?php
+require("sessionstart.php");
+?>
+<html>
+<head>
+<link rel="stylesheet" type="text/css" href="style.css">
+<title>Aegis Home Page</title>
+</head>
+<body>
+<?php
+include("header.php");
+?>
+<h2>Welcome to Aegis</h2>
+<p>This website helps provide support for young adults with autism.</p>
+<p>Aegis is a product of Spectrum Works.</p>
+<p>Find out more about Spectrum Works with this link.</p>
+<p><a href='https://www.spectrumworks.org/'>Spectrum Works</a></p>
+<p>Aegis was programmed by CJ Conti, Kevin Defendre, Daniela Meneses and Marcos Rodriguez.</p>
+<p>The design of this Aegis was created by Michael Joy, CJ Conti, Kevin Defendre and Marcos Rodriguez.</p>
+<p>We hope that you find this website useful!</p>
+</body>
+</html>
