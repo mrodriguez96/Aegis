e33bfd843d6d106339bab17c6887647edce4236f
Marcos Rodriguez <mrodriguezx777@gmail.com>
2020-08-05 16:36:42 -0400

    Create db.php

---
+++ Code/db.php
@@ -0,0 +1,8 @@
+<!--
+Webpage Name:db.php
+Main Developer: CJ Conti
+Primary Function:Provides access to the database for this website.
+-->
+<?php
+$conn=mysqli_connect("localhost", "cjcuser", "computing", "Aegis");
+?>
